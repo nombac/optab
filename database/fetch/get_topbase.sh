f00e27b74f5d756360073bae68dd202c77a26555
Shigenobu Hirose <38897846+nombac@users.noreply.github.com>
2023-11-07 16:40:56 +0900

    small corrections

--- database/fetch/get_topbase.sh
+++ database/fetch/get_topbase.sh
@@ -6,10 +6,10 @@
 dir=./
 #echo ${dir}
 
-#for nz in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 16 18 20 26 ; do
-for nz in 11 ; do
-#    for ne in `seq 1 ${nz}` ; do
-    for ne in 9 ; do
+for nz in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 16 18 20 26 ; do
+#for nz in 11 ; do
+    for ne in `seq 1 ${nz}` ; do
+#    for ne in 9 ; do
 
 	ename=${dir}/elevel.`printf %02d ${nz}`.`printf %02d ${ne}`.dat
 	URL="http://cdsweb.u-strasbg.fr/cgi-bin/topbase/topbase.sh?com=dt&ent=e&nz1=${nz}&nz2=${nz}&ne1=${ne}&ne2=${ne}&is1=1&is2=9&il1=0&il2=9&ip1=0&ip2=1&lv1=0&lv2=0&en1=0.0&en2=0.0&so=s2&te=on&gi=on"
