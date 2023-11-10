15ff6f6553f5452440004a57bdeefcf39f1de216
Shigenobu Hirose <shirose@jamstec.go.jp>
2023-11-10 15:22:33 +0900

    initial commit

---
+++ sample/python/mono.py
@@ -0,0 +1,71 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import argparse
+import subprocess
+
+# Constants
+K_BOL = 1.3806503e-16
+C2 = 1.4387766844642551
+
+def read_mono_hdf5(file_path):
+    with h5py.File(file_path, 'r') as f:
+        return {name: f[name][:] for name in f.keys()}
+
+def generate_plot(dir_path, layer):
+    files = sorted([f for f in os.listdir(os.path.join(dir_path, 'output')) if f.startswith("mono_") and f.endswith(".h5")])
+    nmax = len(files)
+    
+    if layer > nmax - 1:
+        raise ValueError(f'ERROR: layer {layer} > layer max {nmax-1}')
+
+    print(f'Reading {nmax} layers ... ')
+    data = read_mono_hdf5(os.path.join(dir_path, 'output', f'mono_{layer:05d}.h5'))
+    
+    tmp = data['temp']
+    grd = data['grd']
+    abs_ = data['abs']
+    sca = data['sca']
+    cnt = data['cnt']
+    np_ = data['nden']
+    pre = np_[0] * K_BOL * tmp
+
+    grd0 = np.logspace(1, 10, 101)
+    planck = grd0**3 / (np.exp(C2 * grd0 / tmp[0]) - 1)
+    planck /= np.max(planck)
+    
+    ps_name = os.path.join(dir_path, 'output', f'mono_{layer:05d}')
+
+    plt.rcParams.update({'font.size': 16})  # Adjust the number to your preference
+    plt.figure(figsize=(10, 8))
+    plt.loglog(grd, abs_, color='red', label='abs (cnt. + line)')
+    plt.loglog(grd, cnt - sca, color='gray', label='abs (cnt.)')
+    plt.loglog(grd, sca, color='blue', label='sca')
+    plt.loglog(grd0, planck, color='green', linestyle=':', label='Planck')
+    plt.xlabel(r'$\nu$ [cm$^{-1}$]')
+    plt.ylabel(r'$\alpha$ [cm$^{-1}$]')
+    plt.title('Monochromatic Opacity')
+    plt.legend(loc='best')
+    plt.ylim([1e-35,1e10])
+    plt.grid(True)
+    # Add text for tmp and pre
+    plt.text(0.05, 0.15, f'T = {tmp.item():.2f} K', transform=plt.gca().transAxes, fontsize=12)
+    plt.text(0.05, 0.10, f'P = {pre.item():.2e} Ba', transform=plt.gca().transAxes, fontsize=12)
+    plt.show()
+    
+    plt.savefig(f'{ps_name}.pdf', format='pdf', transparent=True)
+    plt.savefig(f'{ps_name}.png', format='png', transparent=True)
+
+    print(f'Output files: {ps_name}.eps, {ps_name}.pdf, {ps_name}.png')
+
+def main():
+    parser = argparse.ArgumentParser(description='Monochromatic Opacity Plotter')
+    parser.add_argument('dir_path', type=str, help='The path to the directory containing HDF5 files')
+    parser.add_argument('layer', type=int, help='Layer number to process and plot')
+    args = parser.parse_args()
+
+    generate_plot(args.dir_path, args.layer)
+
+if __name__ == '__main__':
+    main()
---
+++ sample/python/opac.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import argparse
+
+# Constants
+K_BOL = 1.3806503e-16
+
+def read_mono_hdf5(file_path):
+    try:
+        with h5py.File(file_path, 'r') as f:
+            return {name: f[name][0] for name in f.keys()}
+    except Exception as e:
+        raise RuntimeError(f"Failed to read {file_path}: {e}")
+
+def opac(dir_path, mean, syms):
+    files = [os.path.join(dir_path, 'output', f) for f in os.listdir(os.path.join(dir_path, 'output')) if f.startswith("mono_") and f.endswith(".h5")]
+    nmax = len(files)
+    
+    if nmax == 0:
+        raise ValueError("No HDF5 files found.")
+
+    print(f"Reading {nmax} layers ... ")
+
+    datasets = [read_mono_hdf5(file) for file in files]
+    tmp2 = np.array([data['temp2'] for data in datasets])
+    tmp_tot = np.array([data['temp'] for data in datasets])
+    rho_tot = np.array([data['rho'] for data in datasets])
+    pre_tot = np.array([data['nden'] * K_BOL * data['temp'] for data in datasets])
+    ros_tot = np.log10(np.array([data['ros'] for data in datasets]) / rho_tot)
+    pla_tot = np.log10(np.array([data['plac'] + data['plal'] for data in datasets]) / rho_tot)
+    pla2_tot = np.log10(np.array([data['plac2'] + data['plal2'] for data in datasets]) / rho_tot)
+
+    # Color scaling for visualization
+    vmax, vmin = 7.0, -6.0
+
+    # Title and label mapping
+    titles = {
+        'ross': ('Rosseland-mean opacity', 'log $\kappa$ [cm$^2$/g]'),
+        'pla': ('Planck-mean opacity', 'log $\kappa$ [cm$^2$/g]'),
+        'pla2': (f'Planck-mean opacity at T_rad={int(tmp2[0])}K', 'log $\kappa$ [cm$^2$/g]')
+    }
+
+    if mean not in titles:
+        raise ValueError(f"Unknown mean type: {mean}")
+    title, btitle = titles[mean]
+
+    # Plotting
+    plt.rcParams.update({'font.size': 16})  # Adjust the number to your preference
+    
+    plt.figure(figsize=(10, 8))
+    scatter = plt.scatter(tmp_tot, rho_tot, c=
+                          ros_tot if mean == 'ross' else
+                          pla_tot if mean == 'pla' else
+                          pla2_tot,
+                          s=syms, cmap='viridis', norm=plt.Normalize(vmin=vmin, vmax=vmax), marker='s')
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.xlabel('T [K]')
+    plt.ylabel(r'$\rho$ [g/cm$^3$]')
+    plt.title(title)
+    plt.colorbar(scatter, label=btitle)
+    plt.grid(True)
+    plt.show()
+
+    # Save the plot in different formats
+    file_basename = os.path.join(dir_path, 'output', mean)
+    for ext in ['pdf', 'png']:
+        plt.savefig(f'{file_basename}.{ext}', format=ext, transparent=True)
+        print(f'Output file: {file_basename}.{ext}')
+
+
+def main():
+    # Create the parser
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    # Add the arguments
+    parser.add_argument('dir_path', type=str, help='The path to the directory containing HDF5 files')
+    parser.add_argument('mean', type=str, choices=['ross', 'pla', 'pla2'], help='Type of mean opacity to plot')
+    parser.add_argument('syms', type=int, help='Marker size for scatter plot')
+
+    # Execute the parse_args() method
+    args = parser.parse_args()
+
+    opac(args.dir_path, args.mean, args.syms)
+
+if __name__ == '__main__':
+    main()
